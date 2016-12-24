407c2ffb50483a18549ac286c2e0ef7d1adc0086
Emily Wenberg <emily@praesid.io>
2016-12-23 22:02:17 -0800

    Start on some infrastructure to put images on s3 instead of this repo

---
+++ scripts/upload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# How to use: 
+#  - set up the aws cli for whatever system (requires python and pip)
+#  - run `aws configure --profile upload` and give it the right iam keys
+#  - 
+
+if [ $# -ne 1 ]; then
+    echo 'usage: upload <directory>'
+    exit 1
+fi
+
+directory=$1
+aws --profile upload s3 sync $directory s3://ewenberg-blog-images/$directory
+
